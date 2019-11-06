0b29945a573c984182e6da4437af6483c60b7402
clinton231995 <55691136+clinton231995@users.noreply.github.com>
2019-11-06 01:42:35 +0000

    Over here retrive function is added to complete.
    Over here retrive function is added to complete the python program

--- assign.py
+++ assign.py
@@ -1,27 +1,39 @@
-# -*- coding: utf-8
+def add(n):
+
+    for i in range(n):
+     print("Enter the roll number of the student : ")
+     no = int(input())
+     print("Enter the name of the student : ")
+     name = input()
+     values = [no,name]
+     print("no:",no,"name:",name)
+     array.append(values)
+    return array
 
 
-#creating bubble sorting function
 def bubbleSort(lst1):
-    n=len(lst1)
-    for num in range(n-1,0,-1):
+    for num in range(len(lst1)-1,0,-1):
         for i in range(num):
             if lst1[i]>lst1[i+1]:
                 temp = lst1[i]
                 lst1[i] = lst1[i+1]
                 lst1[i+1] = temp
                 
-array = []
-arr = []
+                
+                
+def retrieve(array,arr):
+ arr1=[]
+ for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[i]==array[j][0]:
+            arr1.append(array[j])
+ return arr1
+
+
+array=[]
+arr=[]
 n = int(input("number of students to add"))
-for i in range(n):
- print("Enter the roll number of the student : ")
- no = int(input())
- print("Enter the name of the student : ")
- name = input()
- values = [no,name]
- print("no:",no,"name:",name)
- array.append(values)
+array=add(n)
 print(array)
 for i in range(len(array)):
  arr.append(array[i][0])
@@ -29,7 +41,6 @@ print(arr)
 
 bubbleSort(arr)
 
-
+arr1=retrieve(array,arr)
 print ("Sorted array is:")
-print (arr) 
-print (arr)
+print (arr1) 
