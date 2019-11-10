243356118621a5ddb05964a68bab3cc56e74fe68
clinton231995 <55691136+clinton231995@users.noreply.github.com>
2019-11-10 18:09:10 +0000

    With init function
    Updated the existing code with the init function.

--- assign.py
+++ assign.py
@@ -1,46 +1,33 @@
-def add(n):
-
-    for i in range(n):
-     print("Enter the roll number of the student : ")
-     no = int(input())
-     print("Enter the name of the student : ")
-     name = input()
-     values = [no,name]
-     print("no:",no,"name:",name)
-     array.append(values)
-    return array
-
-
-def bubbleSort(lst1):
-    for num in range(len(lst1)-1,0,-1):
-        for i in range(num):
-            if lst1[i]>lst1[i+1]:
-                temp = lst1[i]
-                lst1[i] = lst1[i+1]
-                lst1[i+1] = temp
-                
-                
-                
-def retrieve(array,arr):
- arr1=[]
- for i in range(len(arr)):
-    for j in range(len(arr)):
-        if arr[i]==array[j][0]:
-            arr1.append(array[j])
- return arr1
-
-
+class Student:
+    def __init__(self,array=[],lst1=[]):
+        self.array=array
+        self.lst1=lst1
+    
+    def add(self):
+        for i in range(len(self.lst1)-1,0,-1):
+            for j in range(i):
+                if self.lst1[j]>self.lst1[j+1]:
+                    temp = self.lst1[j]
+                    self.lst1[j] = self.lst1[j+1]
+                    self.lst1[j+1] = temp
+        for i in range(len(self.lst1)):
+            for j in range(len(self.lst1)):
+                if self.lst1[i]==self.array[j][0]:
+                    lst2.append(self.array[j])
+        return lst2
 array=[]
-arr=[]
-n = int(input("number of students to add"))
-array=add(n)
-print(array)
+lst1=[]
+lst2=[]
+n=int(input('Enter the number of students to add : '))        
+for i in range(n):
+    print("Enter the roll number of the student : ")
+    no = int(input())
+    print("Enter the name of the student : ")
+    name = input()
+    values = [no,name]
+    print("no:",no,"name:",name)
+    array.append(values)
 for i in range(len(array)):
- arr.append(array[i][0])
-print(arr)
-
-bubbleSort(arr)
-
-arr1=retrieve(array,arr)
-print ("Sorted array is:")
-print (arr1) 
+    lst1.append(array[i][0])
+student=Student(array,lst1)
+print(student.add())
